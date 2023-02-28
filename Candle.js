bab5e66569ba13f45b81b14f10316da5474a0f74
ZenBakr <99152082+ZenBakr@users.noreply.github.com>
2023-02-27 22:52:15 -0500

    Add files via upload

---
+++ Candle.js
@@ -0,0 +1,36 @@
+img="";
+status="";
+
+function back()
+{
+    window.location="index.html";
+}
+
+function preload()
+{
+    img = loadImage('Candle.jpeg');
+}
+
+function setup()
+{
+    canvas = createCanvas(640,420);
+    canvas.center();
+    detection = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+}
